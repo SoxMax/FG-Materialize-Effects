9597f7bcf17ab3748ebdcf36ac0dd85868e439ec
SoxMax <irishsoxmax@gmail.com>
2021-10-11 13:23:18 -0400

    refactor and add base stats

--- scripts/materialize_effects.lua
+++ scripts/materialize_effects.lua
@@ -1,20 +1,29 @@
 
-local function getNormalAc(sCharNode)
+local function getNormalAc(charNode)
     local rRoll = { sDesc = "", sType = "attack" }
-    local nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, nMissChance = ActorManager35E.getDefenseValue(nil, sCharNode, rRoll)
+    local nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, nMissChance = ActorManager35E.getDefenseValue(nil, charNode, rRoll)
     return nDefenseVal + nDefEffectsBonus
 end
 
-local function getTouchAc(sCharNode)
+local function getTouchAc(charNode)
     local rRoll = { sDesc = "[TOUCH]", sType = "attack" }
-    local nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, nMissChance = ActorManager35E.getDefenseValue(nil, sCharNode, rRoll)
+    local nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, nMissChance = ActorManager35E.getDefenseValue(nil, charNode, rRoll)
     return nDefenseVal + nDefEffectsBonus
 end
 
-local function getFlatfootedAc(sCharNode)
+local function getFlatfootedAc(charNode)
     local rRoll = { sDesc = "[FF]", sType = "attack" }
-    local nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, nMissChance = ActorManager35E.getDefenseValue(nil, sCharNode, rRoll)
+    local nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, nMissChance = ActorManager35E.getDefenseValue(nil, charNode, rRoll)
     return nDefenseVal + nDefEffectsBonus
+end
+
+local function getAbilityScoreBonus(charNode, sAbilityScore)
+    local abilityScoreBonus = ActorManager35E.getAbilityBonus(charNode, sAbilityScore) + ActorManager35E.getAbilityEffectsBonus(charNode, sAbilityScore)
+    if abilityScoreBonus >= 0 then
+        return "+" .. abilityScoreBonus
+    else
+        return "" .. abilityScoreBonus
+    end
 end
 
 local function getCharacterNodeByName(sName)
@@ -25,29 +34,55 @@ local function getCharacterNodeByName(sName)
     end
 end
 
-local function processAC(sCommand, sParams)
+local function getCharacterNode(charName)
     local rTarget = nil
     if Session.IsHost then
-        rTarget = getCharacterNodeByName(sParams)
+        rTarget = getCharacterNodeByName(charName)
     else
         rTarget = DB.getChild("charsheet", User.getCurrentIdentity())
     end
+    return rTarget
+end
+
+local function getAcString(rTarget)
+    return "Normal AC:     " .. getNormalAc(rTarget) .. "\r" ..
+           "Flat-footed AC:" .. getFlatfootedAc(rTarget) .. "\r" ..
+           "Touch AC:      " .. getTouchAc(rTarget)
+end
+
+local function getAbilityScoresString(rTarget)
+    return "Strength:     " .. getAbilityScoreBonus(rTarget, "strength") .. "\r" ..
+           "Dexterity:    " .. getAbilityScoreBonus(rTarget, "dexterity") .. "\r" ..
+           "Constitution: " .. getAbilityScoreBonus(rTarget, "constitution") .. "\r" ..
+           "Intelligence: " .. getAbilityScoreBonus(rTarget, "intelligence") .. "\r" ..
+           "Wisdom:       " .. getAbilityScoreBonus(rTarget, "wisdom") .. "\r" ..
+           "Charisma:     " .. getAbilityScoreBonus(rTarget, "charisma")
+end
+
+local function processStats(sCommand, sParams)
+    local rTarget = getCharacterNode(sParams)
 
     if rTarget then
-        local msg = {
+        local statsString = ""
+        if sCommand == "ac" then
+            statsString = getAcString(rTarget)
+        elseif sCommand == "abilityscores" then
+            statsString = getAbilityScoresString(rTarget)
+        elseif sCommand == "materializestats" then
+            statsString = getAbilityScoresString(rTarget) .. "\r" .. getAcString(rTarget)
+        end
+
+        Comm.addChatMessage({
             font = "systemfont",
             icon = DB.getValue(rTarget, "token", ""),
             secret = false,
-            text = DB.getValue(rTarget, "name", "") .. "\n" ..
-            "Normal AC:     " .. getNormalAc(rTarget) .. "\n" ..
-            "Flat-footed AC:" .. getFlatfootedAc(rTarget) .. "\n" ..
-            "Touch AC:      " .. getTouchAc(rTarget)
-        }
-        
-        Comm.addChatMessage(msg)
+            text = DB.getValue(rTarget, "name", "") .. "\r" .. statsString
+        })
     end
 end
 
 function onInit()
-    Comm.registerSlashHandler("ac", processAC)
+    Comm.registerSlashHandler("ac", processStats)
+    Comm.registerSlashHandler("abilityscores", processStats)
+    Comm.registerSlashHandler("materializestats", processStats)
 end
